78146fa0b3318a45639e5fdee6a0c342cf807260
nrb2002 <tshibasubaron@yahoo.fr>
2022-05-20 22:31:00 +0100

    creation of job_applciation
    New update from 20 may 22:30

--- omega_system/index.php
+++ omega_system/index.php
@@ -1,16 +1,38 @@
-<?php
-
-        //Bays @ code
-        //version A0 Oméga System
-        //<!--Navbar-->
-        include('view/navbar.html');
-    //<!--Navbar-->
-
-     include_once('controller/user_authentication.php');
-
-
-        //footer omega système
-        include('view/footer.html');
-
+<!DOCTYPE html>
+<html lang="en">
+        <head>
+        <meta charset="UTF-8">
         
-?>
+
+        <!-- Favicon icon -->
+
+        <link rel="icon" href="assets/images/logo_icon.png" type="image/x-icon">
+
+        <link href="https://fonts.googleapis.com/css?family=Poppins:400,600&display=swap" rel="stylesheet"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+        <link rel="stylesheet" href="../script/Css/style.css">
+        <link rel="stylesheet" href="../../script/Css/B_stylepage.css">
+        </head>
+        <body>
+              <?php
+
+                        //Bays @ code
+                        //version A0 Oméga System
+                        //<!--Navbar-->
+                        include('view/navbar.html');
+                //<!--Navbar-->
+
+                include_once('controller/user_authentication.php');
+
+
+                        //footer omega système
+                        include('view/footer.html');
+
+                        
+                ?>
+
+
+                <script src="script/Js/script.js"></script>
+
+        </body>
+</html>
